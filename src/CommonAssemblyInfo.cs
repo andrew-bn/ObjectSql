53aa13ab663ed9480ea8df6a8f324868e7f6e59b
andrew-bn <mail.andrey.bulygin@gmail.com>
2014-09-30 13:55:13 +0300

    version 2.0.1.3

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -12,5 +12,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.1.2")]
-[assembly: AssemblyFileVersion("2.0.1.2")]
+[assembly: AssemblyVersion("2.0.1.3")]
+[assembly: AssemblyFileVersion("2.0.1.3")]
