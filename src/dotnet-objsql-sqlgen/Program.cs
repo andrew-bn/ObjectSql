ff9e9d7dd7c59f2a666187f727ebc4cba853f5f0
andrew-bn <mail.andrey.bulygin@gmail.com>
2016-05-21 19:59:42 +0300

    sql-gen

--- src/dotnet-objsql-sqlgen/Program.cs
+++ src/dotnet-objsql-sqlgen/Program.cs
@@ -8,33 +8,33 @@ using Microsoft.Extensions.Configuration;
 
 namespace dotnet_objsql_sqlgen
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-	        var curDir = Directory.GetCurrentDirectory();
-			var projectName = curDir.Substring(curDir.LastIndexOf(Path.DirectorySeparatorChar)+1);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var curDir = Directory.GetCurrentDirectory();
+			var projectName = curDir.Substring(curDir.LastIndexOf(Path.DirectorySeparatorChar) + 1);
 
-			if (!File.Exists(Path.Combine(curDir,"project.json")))
+			if (!File.Exists(Path.Combine(curDir, "project.json")))
 				throw new FileNotFoundException("It is expected that objsql-sqlgen utility runs at the root of project folder");
 
 			var files = Directory.GetFiles(curDir, "*.objsql.*.json", SearchOption.AllDirectories);
-	        foreach (var f in files)
-	        {
-		        var dbDir = Path.GetDirectoryName(f);
-		        var configFileName = Path.GetFileName(f);
+			foreach (var f in files)
+			{
+				var dbDir = Path.GetDirectoryName(f);
+				var configFileName = Path.GetFileName(f);
 
-				var ns = projectName + dbDir.Substring(curDir.Length).Replace(Path.DirectorySeparatorChar,'.');
-		        var csFileName = configFileName;
-		        csFileName = Path.Combine(dbDir, csFileName.Substring(0, csFileName.IndexOf(".objsql"))+".cs");
-		        var config = new ConfigurationBuilder()
+				var ns = projectName + dbDir.Substring(curDir.Length).Replace(Path.DirectorySeparatorChar, '.');
+				var csFileName = configFileName;
+				csFileName = Path.Combine(dbDir, csFileName.Substring(0, csFileName.IndexOf(".objsql")) + ".cs");
+				var config = new ConfigurationBuilder()
 											.SetBasePath(dbDir)
 											.AddJsonFile(configFileName)
 											.Build();
 
-		        var cs = config["connectionstring"].Replace("{projectDir}", curDir);
-		        var proceduresName = dbDir.Substring(dbDir.LastIndexOf(Path.DirectorySeparatorChar)+1);
-		        
+				var cs = config["connectionstring"].Replace("{projectDir}", curDir);
+				var proceduresName = dbDir.Substring(dbDir.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+
 
 				Console.Write($@"
 
@@ -42,10 +42,10 @@ ConnetionString: {cs}
 FileName: {csFileName}
 
 Script generation. Please wait...");
-		        File.WriteAllText(csFileName, SchemaGenerator.Generate(ns, proceduresName, cs));
+				File.WriteAllText(csFileName, SchemaGenerator.Generate(ns, proceduresName, cs));
 				Console.WriteLine(" Done");
 			}
-	
-        }
-    }
+
+		}
+	}
 }
