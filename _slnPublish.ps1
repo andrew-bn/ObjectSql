25f0bbd004ef76bbef6bea31e651137d56d5c155
andrew-bn <mail.andrey.bulygin@gmail.com>
2019-03-27 00:57:50 +0200

    new version

--- _slnPublish.ps1
+++ _slnPublish.ps1
@@ -1,21 +1,19 @@
 param (
-    [int]$bn = 0,
-    [string]$branch = "develop",
-    [string]$progetUser = "",
-    [string]$progetPwd = "",
-	[string]$source = "http://proget.regery.com/nuget/backcore"
+    [int]$bn = 5,
+    [string]$branch = "rc3",
+	[string]$nugetApiKey = "",
+	[string]$nugetSource = "http://nuget.regery.net/v3/index.json"
 )
 
 $currentFolder = Get-Location
 $buildNumber = ("{0:D5}" -f $bn)
-$nugetSource = $source
-$nugetOutput = "$currentFolder\nugets"
+$nugetOutput = "$currentFolder/nugets"
 $versionSuffixTemp = "$branch-$buildNumber"
 $versionSuffix = $versionSuffixTemp.Split('/') | Select-Object -Last 1
 $versionPrefix = Get-Content version.txt
-$buildConf = "Debug"
+$buildConf = "Release"
+
 if ("$branch" -eq "master"){
-	$buildConf =  "Release"
 	$versionSuffix = "ztm-$buildNumber"
 }
 
@@ -27,13 +25,23 @@ Write-Host "versionSuffix is:" $versionSuffix -foreground "Yellow"
 Write-Host "nugetSource is:" $nugetSource -foreground "Yellow"
 
 if (Test-Path $nugetOutput){
-     Remove-Item $nugetOutput\*.* -Force -Recurse -ErrorAction SilentlyContinue
+     Remove-Item $nugetOutput/*.* -Force -Recurse -ErrorAction SilentlyContinue
      Start-Sleep -s 1
 }
 
-mkdir $nugetOutput -Force -confirm:$false -ErrorAction SilentlyContinue
+New-Item $nugetOutput -Force -confirm:$false -ErrorAction SilentlyContinue -ItemType directory
+
 
 Push-Location
-
-iex 'dotnet msbuild "/t:Restore;Pack" /p:VersionSuffix=$versionSuffix /p:VersionPrefix=$versionPrefix /p:Configuration=$buildConf /p:OutputPath="$nugetOutput"'
+iex 'dotnet msbuild "/t:Restore;Pack" /p:VersionSuffix=$versionSuffix /p:VersionPrefix=$versionPrefix /p:Configuration=$buildConf'
+Get-ChildItem "src" -Filter *.csproj -Recurse | 
+    Foreach-Object { 
+        $projectPath = Split-Path $_.FullName -Parent 
+        $projectName = Split-Path $projectPath -Leaf
+        Set-Location $projectPath
+		Set-Location bin/$buildConf
+		Write-Host $projectName
+		Write-Host 'dotnet nuget push *$versionSuffix.nupkg -source $nugetSource'
+		iex 'dotnet nuget push *$versionSuffix.nupkg -s $nugetSource -k "$nugetApiKey"'
+    }
 Pop-Location	
